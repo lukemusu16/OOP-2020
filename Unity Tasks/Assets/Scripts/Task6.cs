c3e84718ce1e2cd0b455d7411870212cf0a85d60
lukemusu16 <56021197+lukemusu16@users.noreply.github.com>
2020-10-27 09:39:34 +0100

    Task 6 Finished

---
+++ Unity Tasks/Assets/Scripts/Task6.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task6 : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float moveSpeed;
+    float dirX, dirY;
+
+    private float XMin, XMax, YMin, YMax;
+    public float padding;
+
+    Vector3 newpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 3f;
+        rb = GetComponent<Rigidbody2D>();
+
+        Camera cam = Camera.main;
+        XMin = cam.WorldToViewportPoint(new Vector3(0, 0, 0)).x + padding;
+        XMax = cam.WorldToViewportPoint(new Vector3(1, 0, 0)).x - padding;
+        YMin = cam.WorldToViewportPoint(new Vector3(0, 0, 0)).y + padding;
+        YMax = cam.WorldToViewportPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxis("Horizontal") * moveSpeed;
+        dirY = Input.GetAxis("Vertical") * moveSpeed;
+
+        //newpos = Mathf.Clamp(XMin, XMax);s
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(dirX, dirY, rb.velocity.y);
+    }
+}
---
+++ Unity Tasks/Assets/Scripts/Task7.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Task7 : MonoBehaviour
+{
+    public List<GameObject> TileType = new List<GameObject>();
+    public GameObject[,] squares = new GameObject[16, 16];
+
+    private float XMin, XMax, YMin, YMax;
+    public float padding;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateBoard();
+
+        Camera cam = Camera.main;
+        XMin = cam.WorldToViewportPoint(new Vector3(0, 0, 0)).x + padding;
+        XMax = cam.WorldToViewportPoint(new Vector3(1, 0, 0)).x - padding;
+        YMin = cam.WorldToViewportPoint(new Vector3(0, 0, 0)).y + padding;
+        YMax = cam.WorldToViewportPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateBoard()
+    {
+        
+
+        for (int i = -8; i < 8; i+=2) {
+            for (int j = -8; j < 8; i+=2) {
+
+                Vector3 pos = new Vector3(i, j, 0);
+
+                if (i % 2 != 0 && j % 2 != 0 || i % 2 == 0 && j % 2 == 0)
+                {
+                    squares[i,j] = Instantiate(TileType[0], pos, Quaternion.identity);
+                    
+                }
+                else {
+                    squares[i,j] = Instantiate(TileType[1], pos, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
